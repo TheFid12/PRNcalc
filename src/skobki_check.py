dc221797848ec725c87860e1aa5a3d9e3628c204
TheFid12 <zhenya.bytchkov@mail.ru>
2025-10-03 15:25:31 +0300

    Улучшен вывод ошибок

--- src/skobki_check.py
+++ src/skobki_check.py
@@ -19,12 +19,16 @@ bool: True, если скобки корректны, иначе False.
             stack.append(i)
         elif tokens[i] == ')':
             if not stack:
-                return False
+                print("Закрывающия скобка без открывающей")
+                return False   
             start = stack.pop()
             if not check_skobki(tokens[start+1:i]):
                 return False
             if not check_rpn([t for t in tokens[start+1:i]
                               if t not in {'(', ')'}],0):
+                print("Выражение в скобках не является обратной польской записью")
                 return False
         i += 1
+    if stack:
+        print("Открывающих скобок больше чем закрывающих")
     return not stack
